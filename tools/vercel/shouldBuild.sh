6ff60bee361e5f8fa3e86824ab9a75800a1d1a56
Shoniwa Marovatsanga <stmarovatsanga@gmail.com>
2024-10-03 15:17:45 +0100

    feat: update BRANCH_NAME

--- tools/vercel/shouldBuild.sh
+++ tools/vercel/shouldBuild.sh
@@ -8,19 +8,7 @@ if [[ "$VERCEL_ENV" == "production" ]]; then
   npx nx-ignore $APP
   exit $? # Exit with the same status as NX Ignore
 else
-  BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-  SHOW_CURRENT_BRANCH=$(git branch --show-current)
-  SYMBOLIC_REF=$(git symbolic-ref --short HEAD)
-
-  echo
-  echo "**** Running post-commit hook from branch $BRANCH_NAME"
-  echo
-  echo "**** Running post-commit symbolic-ref hook from branch $SYMBOLIC_REF"
-  echo
-  echo "**** Running post-commit current hook from branch $SHOW_CURRENT_BRANCH"
-  echo
-  echo "**** Running post-commit VERCEL_GIT_COMMIT_REF hook from branch $VERCEL_GIT_COMMIT_REF"
-  echo
+  BRANCH_NAME=$VERCEL_GIT_COMMIT_REF
 
   if [[ "$BRANCH_NAME" == "main" ]]; then
     npx nx-ignore $APP
